99b783a43e4f7a88d43f721202bdccae42f53e5d
Emirhan Uluksar <uluksaremirhan@gmail.com>
2023-07-13 08:50:53 +0300

    car service is created

--- vehicle-management-app/src/app/services/car.service.ts
+++ vehicle-management-app/src/app/services/car.service.ts
@@ -3,15 +3,19 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Car } from '../../store/model/car.model';
 import { tap, map } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class CarService {
-  private apiUrl = `https://localhost:7066/api/Car/GetAll`;
+  private apiUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    console.log(this.apiUrl);
+  }
 
   getCars(): Observable<Car[]> {
-    return this.http.get<{ Cars: Car[] }>(this.apiUrl).pipe(
+    const endpoint = `${this.apiUrl}car/GetAll`;
+    return this.http.get<{ Cars: Car[] }>(endpoint).pipe(
       map((response) => response.Cars),
       tap((cars) => {
         console.log('Cars:', cars);
