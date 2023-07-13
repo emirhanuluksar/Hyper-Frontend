d7a9a2a9c9e390607a08bd45a55ac34f2e1c65fa
Emirhan Uluksar <uluksaremirhan@gmail.com>
2023-07-13 08:50:19 +0300

    car component is created

--- vehicle-management-app/src/app/components/car/car.component.ts
+++ vehicle-management-app/src/app/components/car/car.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Store, select } from '@ngrx/store';
-import { Observable, Subject } from 'rxjs';
+import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { Car } from '../../../store/model/car.model';
 import { getCars } from '../../../store/actions/car.actions';
@@ -8,11 +8,8 @@ import { AppState } from '../../../store/model/app.state';
 
 @Component({
   selector: 'app-car',
-  template: `
-    <div *ngFor="let car of cars">
-      {{ car.color }} Car with {{ car.wheels }} wheels
-    </div>
-  `,
+  templateUrl: './car.component.html',
+  styleUrls: ['./car.component.css'],
 })
 export class CarComponent implements OnInit, OnDestroy {
   private unsubscribe$ = new Subject<void>();
